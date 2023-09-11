08ae99ea2cac0cd395377c02f9236f0f0ad9c236
Zalanooo <113334732+Zalan1012@users.noreply.github.com>
2023-09-11 14:02:53 +0200

    Add files via upload

---
+++ feladat.001.py
@@ -0,0 +1,12 @@
+# feladat001
+"""
+Kérjük be a billentyűzetről a nevünket, és
+írassuk ki a képernyőre
+"""
+"""
+nev = input("Kérek egy nevet")
+print(f"A megadott név a következő: {nev}")
+"""
+vnev = input ("Kérek egy vezetéknevet:")
+knev = input ("kérek egy keresztnevet:" )
+print(f"A megadott név a következő: {vnev} {knev}")
