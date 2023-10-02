d1547bf625ebc1acea62383cb3692627297c6434
Zalanooo <113334732+Zalan1012@users.noreply.github.com>
2023-10-02 14:17:22 +0200

    Add files via upload

---
+++ 9-10-ORA/feladat_gyakorlas.py
@@ -0,0 +1,18 @@
+eletkor = int(input("Kérem, adja meg az életkorát: "))
+
+# Életkor kategória megállapítása
+if eletkor < 0:
+    kategoria = "Még nem születtél meg"
+elif eletkor < 5:
+    kategoria = "Még nem jársz általános iskolába"
+elif eletkor < 14:
+    kategoria = "általános iskolába jársz"
+elif eletkor < 64:
+    kategoria = "tanulsz vagy dolgozol"
+elif eletkor < 65:
+    kategoria = "nyugdíjas"
+else:
+    kategoria = "nyugdíjas"
+
+# Kategória kiírása
+print(f"Az ön életkora a következő kategóriába tartozik: {kategoria}")
