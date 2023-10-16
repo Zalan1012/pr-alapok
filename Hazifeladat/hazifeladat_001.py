28eb54010d510b2ca2e66be6519c05bf4efa679e
Zalanooo <113334732+Zalan1012@users.noreply.github.com>
2023-10-16 13:33:52 +0200

    Add files via upload

---
+++ Hazifeladat/hazifeladat_001.py
@@ -0,0 +1,10 @@
+# házi feladat 1
+
+válasz = input("Jó napja van? ")
+if válasz == "igen":
+    print("Örülök, hogy jó napja van, remélem több ilyen lesz!")
+elif válasz == "nem":
+    print("Sajnálom, remélem holnap jobb napja lesz!")
+
+else:
+    print("Sajnos nem értem a válaszodat.")
---
+++ Hazifeladat/hazifeladat_002.py
@@ -0,0 +1,10 @@
+# házi feladat 2
+
+sugar = int(input("Kérem adjon meg egy számot (sugár/cm): "))
+pi = 3.14 
+
+kerulet = 2 * sugar * pi
+terulet = sugar**2 * pi
+
+print(f"A kör kerülete: {kerulet} cm")
+print(f"A kör területe: {terulet} cm²")
