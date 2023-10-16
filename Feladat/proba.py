14a28d17c91d4f0dec1c564e16642b2a24f33b9b
Zalanooo <113334732+Zalan1012@users.noreply.github.com>
2023-10-16 14:14:53 +0200

    Add files via upload

---
+++ Feladat/proba.py
@@ -0,0 +1,15 @@
+#Eljárás
+
+def menut_kiir(tipus):
+    """
+    A menü megjeleítése a képernyőn
+    """
+    if tipus == 2:
+        print("1. Új adat bevitele")
+        print("2. Kilépés a programmból")
+    if tipus == 3:
+        print("1. Új adat bevitele")
+        print("2. Adatok módosítása")
+        print("3. Kilépés a programmból")
+
+#Eljárás hívása
