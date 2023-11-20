4d88c5dd8bb81ae124cfea9df97b4670e90fe7d1
Zalanooo <113334732+Zalan1012@users.noreply.github.com>
2023-11-20 14:20:13 +0100

    Add files via upload

---
+++ Feladat/feladat_023.py
@@ -0,0 +1,10 @@
+#feladat_023
+
+honapok = ["Január", "Február", "Március", "Április", "Május"]
+
+print(honapok)
+print(("!").join(honapok))
+
+print(len(honapok))
+print(honapok[0])
+print(f"A lista legnagyobb indexe::")
