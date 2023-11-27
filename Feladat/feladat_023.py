63554b4714b9fb32516d43fef326551ef601cfe5
Zalanooo <113334732+Zalan1012@users.noreply.github.com>
2023-11-27 13:43:16 +0100

    Update feladat_023.py

--- Feladat/feladat_023.py
+++ Feladat/feladat_023.py
@@ -1,10 +1,46 @@
-#feladat_023
+# feladat_023
 
-honapok = ["Január", "Február", "Március", "Április", "Május"]
-
+honapok = ['január', 'február', 'március','április', 'május']
+"""
+lista = [1, 2, 3, 4]
+"""
 print(honapok)
-print(("!").join(honapok))
+# print((' ').join(honapok))
 
+print(len(honapok))  #Len = hossz
+#print(honapok[5]
+print(f'A lista utolsó legnagyobb indexe: {len(honapok) - 1}')
+
+honapok = ['január', 'február','március', 'április']
+
+  # lista kiíratása
+print(honapok)
+
+  # join(): a lista elemeit egy sztringgé fűzi össze
+  # a megadott elválasztó karakterekkel tagolva
+print(', '.join(honapok))
+  
+  # lista hossza: len( )
 print(len(honapok))
+  
+  # a 0. indexű az első elem
 print(honapok[0])
-print(f"A lista legnagyobb indexe::")
+  
+  # jelen esetben a 3. indexű az utolsó elem
+print(honapok[3])
+  
+  # túlindexelés, hibát okoz!
+  # ebben a listában az utolsó elem indexe: 3
+  # print(honapok[4]) <- HIBÁS!!!
+  
+  # az 1-es és 2-es indexű elemek kiíratása
+print(honapok[1:3])
+  
+  # az elejétől a 2-es indexű elemmel bezárólag
+print(honapok[:3])
+  
+  # 2-es indexű elemtől a végéig
+print(honapok[2:])
+  
+  # hátulról az első, vagyis az utolsó elem
+print(honapok[-1])      
